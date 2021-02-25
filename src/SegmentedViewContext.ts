cc4e2d44b7cdb902519cbf4b2a975b18ee8903f0
Pedro <pedrobermoreira@gmail.com>
2021-02-25 16:24:41 -0300

    feat: add SegmentedViewContext

---
+++ src/SegmentedViewContext.ts
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Animated } from 'react-native'
+
+import { ScrollRef, SetIndex } from './types'
+
+export type SegmentedViewContextType = {
+  headerHeight: number
+  controlHeight: number
+  containerHeight: number
+  scrollY: Animated.Value
+  setRef: (ref: ScrollRef, index: number) => void
+  lazy: boolean
+  index: Animated.Value
+  initialIndex: number
+  setIndex: SetIndex
+  prevIndex: React.MutableRefObject<number>
+  trackIndex: React.MutableRefObject<number>
+  translateY: Animated.AnimatedInterpolation
+}
+
+export const SegmentedViewContext = React.createContext<
+  SegmentedViewContextType | undefined
+>(undefined)
+
+export function useSegmentedViewContext(): SegmentedViewContextType {
+  const c = React.useContext(SegmentedViewContext)
+  if (!c)
+    throw new Error(
+      'useSegmentedViewContext must be inside a SegmentedViewContext'
+    )
+  return c
+}
